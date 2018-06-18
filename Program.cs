8d8bcaf6658c9968a0f46ea2edd90574368f485e
William Lim <manvinedi@gmail.com>
2018-06-19 00:56:54 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TeleprompterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        // The IEnumerable<T> interface is defined in the
+        // System.Collections.Generic namespace.
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            // The File class is defined in the System.IO namespace.
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
